7a4edbcc68700faab6718ee286c09ad9586aaa8e
Jerry Lundström <lundstrom.jerry@gmail.com>
2020-04-16 10:45:12 +0200

    Test
    - `tests`: Use static files for client subnet authority

--- tests/test.sh
+++ tests/test.sh
@@ -12,7 +12,7 @@ dsc-datatool \
   --transform ";ReRanger;edns_bufsiz,priming_queries;key=low;range=/512;pad_to=5;allow_invalid_keys=1" \
   --transform ";ReRanger;priming_responses;key=low;range=/128;pad_to=4" \
   --transform ";NetRemap;client_subnet,client_subnet2,client_addr_vs_rcode,ipv6_rsn_abusers;net=16" \
-  --generator ";client_subnet_authority;fetch=yes" \
+  --generator ";client_subnet_authority;csv=ipv4-address-space.csv;csv=ipv6-unicast-address-assignments.csv" \
   --xml ./1563520620.dscdata.xml > test.out
 
 diff -u test.gold test.out
